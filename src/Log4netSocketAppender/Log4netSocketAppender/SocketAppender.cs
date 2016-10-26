9688f15b6843a4a131876060405c9c9bab012b57
ugurozsahin <ugurozsahin@live.com>
2016-10-27 00:42:39 +0300

    First Commit For log4net Socket Appender (Logstash, Tcp, Udp vs.)

---
+++ src/Log4netSocketAppender/Log4netSocketAppender/SocketAppender.cs
@@ -0,0 +1,99 @@
+﻿using log4net.Core;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace log4net.Appender
+{
+    public class SocketAppender : AppenderSkeleton
+    {
+        public SocketAppender()
+        {
+            AddressFamily = AddressFamily.InterNetwork;
+            SocketType = SocketType.Stream;
+            ProtocolType = ProtocolType.Tcp;
+            ConAttemptsCount = 3;
+            ConAttemptsWaitingTimeMilliSeconds = 1000;
+            UseThreadPoolQueue = false;
+        }
+        public string RemoteAddress { get; set; }
+        public int RemotePort { get; set; }
+        public bool DebugMode { get; set; }
+        public AddressFamily AddressFamily { get; set; }
+        public SocketType SocketType { get; set; }
+        public ProtocolType ProtocolType { get; set; }
+        public int ConAttemptsCount { get; set; }
+        public int ConAttemptsWaitingTimeMilliSeconds { get; set; }
+        public bool UseThreadPoolQueue { get; set; }
+
+
+        private Socket _socket;
+
+        public override void ActivateOptions()
+        {
+            var retryCount = 0;
+            while (++retryCount <= ConAttemptsCount)
+            {
+                try
+                {
+                    _socket = new Socket(AddressFamily, SocketType, ProtocolType);
+                    _socket.Connect(RemoteAddress, RemotePort);
+                    break;
+                }
+                catch (ArgumentNullException argumentNullException)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", argumentNullException);
+                }
+                catch (SocketException socketException)
+                {
+                    Console.WriteLine("SocketException : {0}", socketException);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", exception);
+                }
+                Thread.Sleep(ConAttemptsWaitingTimeMilliSeconds);
+            }
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (UseThreadPoolQueue)
+                ThreadPool.QueueUserWorkItem(state => AppendLog(loggingEvent));
+            else
+                AppendLog(loggingEvent);
+
+        }
+
+        private void AppendLog(LoggingEvent loggingEvent)
+        {
+            var rendered = string.Empty;
+
+            if (_socket.Connected)
+            {
+                rendered = RenderLoggingEvent(loggingEvent);
+
+                var msg = Encoding.UTF8.GetBytes(rendered);
+
+                var bytesSent = _socket.Send(msg);
+
+                if (DebugMode)
+                {
+                    Console.WriteLine("- Bytes sent: " + bytesSent);
+                }
+            }
+            else
+            {
+                Console.WriteLine("[UNSUCCESSFULL]:: " + rendered);
+            }
+        }
+
+        protected override void OnClose()
+        {
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+    }
+}
+
---
+++ src/Log4netSocketAppender/Log4netSocketAppender/readme.txt
@@ -0,0 +1,15 @@
+﻿Sample log4net Appender Configuration
+
+	<appender name="SocketAppender" type="log4net.Appender.SocketAppender">
+	  <RemoteAddress value="192.168.99.100"/>
+	  <RemotePort value="5000" />
+	  <AddressFamily value="InterNetwork" />
+	  <SocketType value="Stream" />
+	  <ProtocolType value="Tcp" />
+	  <ConAttemptsCount value="5" />
+	  <ConAttemptsWaitingTimeMilliSeconds value="3000" />
+	  <UseThreadPoolQueue value="true" />
+	  <layout type="log4net.Layout.PatternLayout, log4net">
+		<conversionPattern value="%property{log4net:HostName} %level %date{ISO8601} %thread %logger - %message%newline" />
+	  </layout>
+	</appender>
